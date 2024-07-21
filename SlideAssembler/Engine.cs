c29902de4222fdc88315fef7c4a6f6f531f84e1a
MAIN\20200045 <20200045@students.htl-perg.ac.at>
2024-07-21 19:42:25 +0200

    Implmented Engine

---
+++ SlideAssembler/Engine.cs
@@ -0,0 +1,38 @@
+﻿using ShapeCrawler;
+
+namespace SlideAssembler;
+
+
+class SlideAssembler
+{
+    private readonly Presentation presentation;
+
+    private SlideAssembler(Presentation presentation) { this.presentation = presentation; }
+
+    public static Presentation Load(Stream stream)
+    { // Loads presantion from Stream
+
+        return new Presentation(stream);
+    }
+
+    public void Save(Stream stream) // Saves prestation in Stream 
+    {
+        presentation.SaveAs(stream);
+    }
+
+    public SlideAssembler Apply(params IPrestationOperation[] operations)
+    {
+        foreach (var operation in operations)
+        {
+            operation.Apply(presentation);
+        }
+        return this;
+    }
+
+}
+
+interface IPrestationOperation
+{
+    void Apply(Presentation presentation);
+}
+
