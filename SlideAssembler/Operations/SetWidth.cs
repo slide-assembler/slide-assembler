10ca63e779a6bea6bcf8baaada569aea5e5efe98
MAIN\20200045 <20200045@students.htl-perg.ac.at>
2024-07-25 23:20:04 +0200

    implmnted SetWidth

---
+++ SlideAssembler/Operations/SetWidth.cs
@@ -0,0 +1,30 @@
+﻿using ShapeCrawler;
+
+namespace SlideAssembler.Operations
+{
+    public class SetWidth : IPrestationOperation
+    {
+        private readonly string name;
+        private decimal data;
+
+        public SetWidth(string name, decimal data)
+        {
+            this.name = name;
+            this.data = data;
+        }
+        public void Apply(Presentation presentation)
+        {
+            foreach (var slide in presentation.Slides)
+            {
+                foreach (var shape in slide.Shapes)
+                {
+                    if (shape.Name.Equals(name))
+                    {
+                        shape.Width = data;
+                        shape.TextFrame.Text = data.ToString();
+                    }
+                }
+            }
+        }
+    }
+}
