111479b36ef3a4e9ed07a33f5342df110789ec06
stefan <20200079@students.htl-perg.ac.at>
2024-08-19 12:06:59 +0200

    implemented first Unit tests (StandartTest and NoPlaceholderTest)

--- TestSlideAssembler/SetWidthUnitTests.cs
+++ TestSlideAssembler/SetWidthUnitTests.cs
@@ -1,3 +1,6 @@
+using Microsoft.VisualStudio.TestPlatform.Utilities;
+using SlideAssembler;
+
 namespace TestSlideAssembler
 {
     [TestClass]
@@ -32,6 +35,38 @@ namespace TestSlideAssembler
             slideAssembler.Apply(new SetWidth("MittelwertRechteck", (Decimal)data.Mittelwert))
                 .Apply(new SetWidth("MaximumRechteck", (Decimal)data.Maximum))
                 .Apply(new SetWidth("MaximumRechteck", (Decimal)data.Minimum))
+                .Save(output);
+        }
+
+        [TestMethod]
+        public void NullPlaceholderTest()
+        {
+            var values = new[] { 0.82, 0.88, 0.64, 0.79, 0.31 };
+
+            // Platzhalter
+            var data = new
+            {
+                Titel = $"Messwerte vom {DateTime.Now:d}",
+                Benutzer = new
+                {
+                    Vorname = "Philipp",
+                    Nachname = "Kunnert"
+                },
+                Minimum = values.Min(),
+                Mittelwert = values.Average(),
+                Maximum = values.Max(),
+                Werte = values
+            };
+
+            using var template = File.OpenRead("Template.pptx");
+            using var output = new FileStream("Output.pptx", FileMode.Create, FileAccess.ReadWrite);
+
+            SlideAssembler.SlideAssembler slideAssembler = SlideAssembler.SlideAssembler.Load(template);
+
+            slideAssembler = slideAssembler.Apply(new FillPlaceHolders(data));
+            slideAssembler.Apply(new SetWidth("MittelwertRechteck", (Decimal)data.Mittelwert))
+                .Apply(new SetWidth(null, (Decimal)data.Maximum))
+                .Apply(new SetWidth(null, (Decimal)data.Minimum))
                 .Save(output);
         }
     }
