6c3a12c29aa0f8b9dc13a73303e68b617be4d241
MAIN\20200045 <20200045@students.htl-perg.ac.at>
2024-09-11 11:58:13 +0200

    started with FillCharts

---
+++ SlideAssembler/Operations/FillChart.cs
@@ -0,0 +1,21 @@
+﻿using ShapeCrawler;
+using SlideAssembler;
+
+
+public class FillChart : IPresentationOperation
+{
+    private readonly object data;
+    private bool ignoreMissingData;
+
+    public FillChart(object data, bool ignoreMissingData)
+    {
+        this.data = data;
+        this.ignoreMissingData = ignoreMissingData;
+    }
+
+    public void Apply(Presentation presentation)
+    {
+        throw new NotImplementedException();
+    }
+}
+
