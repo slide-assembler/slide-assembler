1004e0ce3194b98c83c2593ed0fb3e7f2c6d781d
stefan <20200079@students.htl-perg.ac.at>
2024-10-16 08:45:10 +0200

    Edited ModifyObject so we can use IgnoreMissingData

--- SlideAssembler/Operations/ModifyObject.cs
+++ SlideAssembler/Operations/ModifyObject.cs
@@ -1,12 +1,23 @@
 ﻿using ShapeCrawler;
 using SlideAssembler;
 
-
-
-public class ModifyObject(string name, Action<IShape> action) : IPrestationOperation
+public class ModifyObject : IPrestationOperation
 {
+    private readonly string name;
+    private readonly Action<IShape> action;
+    private readonly bool ignoreMissingData;
+
+    public ModifyObject(string name, Action<IShape> action, bool ignoreMissingData = false)
+    {
+        this.name = name;
+        this.action = action;
+        this.ignoreMissingData = ignoreMissingData;
+    }
+
     public void Apply(Presentation presentation)
     {
+        bool shapeFound = false;
+
         foreach (var slide in presentation.Slides)
         {
             foreach (var shape in slide.Shapes)
@@ -14,8 +25,14 @@ public class ModifyObject(string name, Action<IShape> action) : IPrestationOpera
                 if (shape.Name.Equals(name))
                 {
                     action(shape);
+                    shapeFound = true;
                 }
             }
+        }
+
+        if (!shapeFound && !ignoreMissingData)
+        {
+            throw new InvalidDataException("Object not found!");
         }
     }
 }
