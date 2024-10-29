11cdc10146769234b1ee105162576c3471fbc8e3
MAIN\20200045 <20200045@students.htl-perg.ac.at>
2024-10-29 16:11:05 +0100

    implmented ReplaceImage

---
+++ SlideAssembler/Operations/ReplaceImage.cs
@@ -0,0 +1,40 @@
+﻿using ShapeCrawler;
+using SlideAssembler;
+
+
+public partial class ReplaceImage : IPresentationOperation
+{
+    private readonly Object data;
+    private bool ignoreMissingData;
+
+    public ReplaceImage(Object data, bool ignoreMissingData = false)
+    {
+        this.data = data;
+        this.ignoreMissingData = ignoreMissingData;
+    }
+    public void Apply(Presentation presentation)
+    {
+        foreach (var slide in presentation.Slides)
+        {
+            foreach (var shape in slide.Shapes)
+            {
+                if (shape is IPicture)
+                {
+                    UpdateImage((IPicture)shape);
+                }
+            }
+        }
+    }
+
+    private void UpdateImage(IPicture image)
+    {
+        var property = data.GetType().GetProperty(image.Name);
+
+        if (property != null && property.PropertyType == typeof(Stream))
+        {
+            Stream stream = (Stream)property.GetValue(data);
+            image.Image.Update(stream);
+        }
+    }
+}
+
