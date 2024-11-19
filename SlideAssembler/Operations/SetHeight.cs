f081789b69f751a3268e703b7f7f0649d4db7655
MAIN\20200045 <20200045@students.htl-perg.ac.at>
2024-11-19 17:53:26 +0100

    implemented setHeight

---
+++ SlideAssembler/Operations/SetHeight.cs
@@ -0,0 +1,40 @@
+﻿using SlideAssembler;
+
+public class SetHeight : IPresentationOperation
+{
+    private readonly string name;
+    private readonly decimal height;
+
+    public SetHeight(string name, decimal height)
+    {
+        if (height < 0.0m)
+        {
+            throw new ArgumentOutOfRangeException("Height has to be >= 0", nameof(height));
+        }
+
+        this.name = name;
+        this.height = height;
+    }
+
+
+    void IPresentationOperation.Apply(PresentationContext context)
+    {
+        var shapeFound = false;
+
+        foreach (var slide in context.Presentation.Slides)
+        {
+            var shape = slide.Shapes.FirstOrDefault(s => s.Name == name);
+            if (shape is not null)
+            {
+                shape.Height = height;
+                shapeFound = true;
+            }
+        }
+
+        if (!shapeFound && context.ThrowOnError)
+        {
+            throw new InvalidDataException($"Shape '{name}' not found.");
+        }
+    }
+}
+
